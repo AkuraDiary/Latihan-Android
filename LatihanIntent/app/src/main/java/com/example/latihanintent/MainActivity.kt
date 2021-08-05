9ab1ef8bb15bdf5f28ce252fc4e08cd69957a995
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-08-05 14:15:49 +0700

    first commit

---
+++ LatihanIntent/app/src/main/java/com/example/latihanintent/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.latihanintent
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //variable untuk meng anu tombol move activity
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        //variable untuk meng anu tombol move activity with data
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_move_activity->{
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)//parameter 1=konteks, 2=tujuan
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data ->{
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWidthDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWidthDataActivity.EXTRA_NAME, "DicodingAcademy Boy") //memasukkan datanya
+                moveWithDataIntent.putExtra(MoveWidthDataActivity.EXTRA_AGE, 5)
+            }
+        }
+    }
+}
---
+++ LatihanIntent/app/src/main/java/com/example/latihanintent/MoveWidthDataActivity.kt
@@ -0,0 +1,24 @@
+package com.example.latihanintent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MoveWidthDataActivity : AppCompatActivity() {
+    companion object{
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_width_data)
+
+        val tvDataRecieved: TextView = findViewById(R.id.tv_data_recieved)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getStringExtra(EXTRA_AGE)
+
+        val text = "Name : $name, Your Age : $age"
+        tvDataRecieved.text = text
+    }
+}
