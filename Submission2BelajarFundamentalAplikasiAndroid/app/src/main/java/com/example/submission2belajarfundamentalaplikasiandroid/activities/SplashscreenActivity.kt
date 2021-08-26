ae3ffd1740dc4ab315d27c46a9d2c20bf9ff7d6f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-08-26 19:53:51 +0700

    tambahan animasi progres bar di splash screen

--- Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/activities/SplashscreenActivity.kt
+++ Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/activities/SplashscreenActivity.kt
@@ -1,9 +1,11 @@
 package com.example.submission2belajarfundamentalaplikasiandroid.activities
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.submission2belajarfundamentalaplikasiandroid.databinding.ActivitySplashscreenBinding
+import kotlinx.android.synthetic.main.activity_splashscreen.*
 import java.lang.Exception
 
 class SplashscreenActivity : AppCompatActivity() {
@@ -13,12 +15,20 @@ class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding_splash = ActivitySplashscreenBinding.inflate(layoutInflater)
+
         setContentView(binding_splash.root)
+
+        // animasi progress bar
+        progressBar.max = 100
+        val barAnimation = ObjectAnimator.ofInt(progressBar, "progress", 100)
+        barAnimation.duration = 3000
+        // start the progress bar animation
+        barAnimation.start()
 
         val background = object : Thread(){
             override fun run(){
                 try{
-                    // do some code here while loading
+                    /* do some code here while loading */
                     sleep(3000)
                     // continue to the next activity
                     val intent = Intent(baseContext, MainActivity::class.java)
