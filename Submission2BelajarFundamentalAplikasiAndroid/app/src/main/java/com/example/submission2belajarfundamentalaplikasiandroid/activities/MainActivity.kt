56689f86c44c21a8a859df6671f28c8b5217144a
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-08-28 01:43:12 +0700

    update

--- Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/activities/MainActivity.kt
+++ Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/activities/MainActivity.kt
@@ -1,13 +1,53 @@
 package com.example.submission2belajarfundamentalaplikasiandroid.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.submission2belajarfundamentalaplikasiandroid.R
+import com.example.submission2belajarfundamentalaplikasiandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController : NavController
+    private lateinit var appBarrConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        navController = findNavController(R.id.nav_host_fragment_container)
+
+        appBarrConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarrConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp(appBarrConfiguration)
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item:MenuItem): Boolean{
+        if(item.itemId == R.id.menu_setting){
+            val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
