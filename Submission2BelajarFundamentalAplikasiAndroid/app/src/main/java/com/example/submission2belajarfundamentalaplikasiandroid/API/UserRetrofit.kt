a02dba43bca9264a7314eceb75f1fd206a7e6db0
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-08-27 23:36:29 +0700

    update

--- Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/API/UserRetrofit.kt
+++ Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/API/UserRetrofit.kt
@@ -1,19 +1,44 @@
 package com.example.submission2belajarfundamentalaplikasiandroid.API
-
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.Dispatchers
 import com.example.submission2belajarfundamentalaplikasiandroid.others.ResourceStats
 
 object UserRetrofit {
-    fun searchForUser(query: String) = liveData(Dispatchers.IO) {
+    fun searchForUser(Query: String) = liveData(Dispatchers.IO) {
         emit(ResourceStats.onLoading(null))
         try {
-            val userSearch = RConfig.API_Client.searchForUsers(query)
+            val userSearch = RConfig.API_Client.searchForUsers(Query)
             emit(ResourceStats.onSuccess(userSearch.items_res))
-        } catch (exception: Exception){
-            emit(ResourceStats.onError(null, exception.message ?: "Error"))
+        } catch (e: Exception){
+            emit(ResourceStats.onError(null, e.message ?: "Error occurred while searching for user"))
         }
     }
 
+    fun getUserDetail(Username:String) = liveData(Dispatchers.IO){
+        emit(ResourceStats.onLoading(null))
+        try{
+            emit(ResourceStats.onSuccess(RConfig.API_Client.userDetails(Username)))
+        }catch (e:Exception){
+            emit(ResourceStats.onError(null, e.message?: "Error occurred while get user detail"))
+        }
+    }
+
+    fun getUserFollowers(Username:String) = liveData(Dispatchers.IO){
+        emit(ResourceStats.onLoading(null))
+        try{
+            emit(ResourceStats.onSuccess(RConfig.API_Client.userDetails(Username)))
+        }catch (e:Exception){
+            emit(ResourceStats.onError(null, e.message?: "Error occurred while get user followers"))
+        }
+    }
+
+    fun getUserFollowing(Username:String) = liveData(Dispatchers.IO){
+        emit(ResourceStats.onLoading(null))
+        try{
+            emit(ResourceStats.onSuccess(RConfig.API_Client.userDetails(Username)))
+        }catch (e:Exception){
+            emit(ResourceStats.onError(null, e.message?: "Error occurred while get user following"))
+        }
+    }
 
 }
