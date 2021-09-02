ed42716c0035f4ce861f5e8fdefd93ab96f10c59
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-09-02 21:30:52 +0700

    optimalisasi tahap 2 part 2

--- Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/fragment/FragmentDetail.kt
+++ Submission2BelajarFundamentalAplikasiAndroid/app/src/main/java/com/example/submission2belajarfundamentalaplikasiandroid/fragment/FragmentDetail.kt
@@ -34,7 +34,9 @@ class FragmentDetail : Fragment() {
     ): View {
         bindingDetail = FragmentDetailBinding.inflate(layoutInflater, container, false)
         bindingDetail.lifecycleOwner = viewLifecycleOwner
+
         observeDetail()
+
         return bindingDetail.root
     }
 
