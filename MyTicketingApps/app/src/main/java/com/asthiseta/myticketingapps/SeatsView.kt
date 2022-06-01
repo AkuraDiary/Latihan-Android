a0ec17cf641cd4da2cdc48d144060c05ea2b4b55
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-01 10:45:32 +0700

    added new colors, and custom seats view class

---
+++ MyTicketingApps/app/src/main/java/com/asthiseta/myticketingapps/SeatsView.kt
@@ -0,0 +1,19 @@
+package com.asthiseta.myticketingapps
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class SeatsView : View {
+    constructor(context : Context) : super(context)
+
+    constructor(context : Context, attrs : AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr : Int) : super
+        (
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+}
