5278eb9d500b2c10021b52180ab1b25853c4bb98
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-01 10:53:20 +0700

    added arraylist of seats

--- MyTicketingApps/app/src/main/java/com/asthiseta/myticketingapps/SeatsView.kt
+++ MyTicketingApps/app/src/main/java/com/asthiseta/myticketingapps/SeatsView.kt
@@ -16,4 +16,16 @@ class SeatsView : View {
         defStyleAttr
     )
 
+    private val seats: ArrayList<Seat> = arrayListOf(
+        Seat(id = 1, name = "A1", isBooked = false),
+        Seat(id = 2, name = "A2", isBooked = false),
+        Seat(id = 3, name = "B1", isBooked = false),
+        Seat(id = 4, name = "B2", isBooked = false),
+        Seat(id = 5, name = "C1", isBooked = false),
+        Seat(id = 6, name = "C2", isBooked = false),
+        Seat(id = 7, name = "D1", isBooked = false),
+        Seat(id = 8, name = "D2", isBooked = false),
+
+    )
+
 }
