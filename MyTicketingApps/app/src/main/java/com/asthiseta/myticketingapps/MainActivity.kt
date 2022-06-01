8ff8355a9119f521f329de05e687824cc7e1f007
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-06-01 16:32:38 +0700

    finally added it into main activity

--- MyTicketingApps/app/src/main/java/com/asthiseta/myticketingapps/MainActivity.kt
+++ MyTicketingApps/app/src/main/java/com/asthiseta/myticketingapps/MainActivity.kt
@@ -2,10 +2,25 @@ package com.asthiseta.myticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi anda nomor ${it.name}", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Anda belum memilih kursi", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
